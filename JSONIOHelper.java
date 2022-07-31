8c950cfb87be47d15dd1fdbb3a8728182bb0e176
Meadhbh O’Brien <41460339+meadhbho@users.noreply.github.com>
2022-07-31 16:08:22 +0100

    Update JSONIOHelper.java

--- JSONIOHelper.java
+++ JSONIOHelper.java
@@ -10,21 +10,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 
-
- 
-//Helper class to help load data into JSON files
-
 public class JSONIOHelper {
 	
-		// Data structure will be a JSON object
-			// Want to have two levels (parent - child(x2)) so will create three objects
 	
 		JSONObject rootObject;
 		JSONObject documentsObject;
 		JSONObject lemmasObject;
 		
 	
-	//Create a method to create the JSON structure of two objects being the children of another object
 
 	public void CreateBasicsJSONStructure() {
 		
@@ -32,9 +25,7 @@ public class JSONIOHelper {
 		documentsObject = new JSONObject();
 		lemmasObject = new JSONObject();
 		
-		// documents object is the child of the rootObject
-		// key = "documents"
-		// value = documentsObject
+
 		rootObject.put("documents", documentsObject);
 		rootObject.put("lemmas", lemmasObject);
 		
@@ -53,7 +44,6 @@ public class JSONIOHelper {
 	
 	public void SaveJSON(String filename) {
 		String JSONString = rootObject.toJSONString();
-		//FileWriter class
 		try(FileWriter writer = new FileWriter(filename)) {
 			writer.write(JSONString);
 			System.out.println("JSON File saved successfully");
@@ -66,10 +56,6 @@ public class JSONIOHelper {
 	}
 	
 	
-// 	LoadJSON method does the following:	
-//	Initialise the basic JSON structure
-//	Open the JSON file and load in the text within
-//	Parse that text back into Java.
 	public void LoadJSON(String filename) {
 		
 		CreateBasicsJSONStructure();
@@ -91,7 +77,6 @@ public class JSONIOHelper {
 		}
 		
 		catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -116,7 +101,6 @@ public class JSONIOHelper {
 	}
 	
 	
-	//Block 2 - Text cleaning
 	public void AddLemmasToJSONStructure(ConcurrentHashMap<String,String>lemmas){
 		
 
@@ -128,7 +112,6 @@ public class JSONIOHelper {
 		
 	}
 	
-	//Block 2 - Text cleaning
 	public ConcurrentHashMap<String, String>GetLemmasFromJSONStructure(){
 		
 		ConcurrentHashMap<String, String>lemmas = new ConcurrentHashMap<String, String>();
